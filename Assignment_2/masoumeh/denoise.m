08ebbaecf6e636e8dc9d734bb0b760e3b9f4f885
mbakhtiariz <mbakhtiariz@gmail.com>
2018-03-05 23:56:27 +0100

    improvement

--- Assignment_2/masoumeh/denoise.m
+++ Assignment_2/masoumeh/denoise.m
@@ -3,12 +3,12 @@ kernel_size = varargin{1};
 switch kernel_type
     case 'box'
        box_filter = ones(kernel_size)/(kernel_size.^2);
-       imOut = imfilter(image, box_filter);
+       imOut = imfilter(image, box_filter,'conv');
     case 'median'
         imOut = medfilt2(image, [kernel_size kernel_size]);
     case 'gaussian'
         sigma = varargin{2};
         filter = gauss2D(sigma, kernel_size);
-        imOut = imfilter(image, filter);
+        imOut = imfilter(image, filter,'conv');
 end
 end
