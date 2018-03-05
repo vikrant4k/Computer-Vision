99188be47ed814c60a10148b98683138ebbed7fc
vikrant4k <vikrant4.k@gmail.com>
2018-03-06 00:16:31 +0100

    Lucas Kande
    licas

--- Assignment_3/lucas_kanade.m
+++ Assignment_3/lucas_kanade.m
@@ -1,34 +1,35 @@
 function[u,v]=lucase_kanade(im1,im2,window_size)
 [gx1,gy1]=gradient(im1);
 gt=im1-im2;
-size(gx1)
 [h,w]=size(im1);
 u=double(zeros(h,w));
 v=double(zeros(h,w));
-size(u)
-for i=1:h
-    for j=1:w
+count=1
+for i=1:window_size:h
+    for j=1:window_size:w
         A=double(zeros(window_size,2));
         b=double(zeros(window_size,1));
         counter=1;
-        if((i+window_size)<=h && (j+window_size)<=w)
-        for k=i:i+window_size
-            for l=j:j+window_size
+        for k=i:min(i+window_size,h)
+            for l=j:min(j+window_size,w)
                 A(counter,1)=gx1(k,l);
                 A(counter,2)=gy1(k,l);
                 b(counter,1)=gt(k,l);
                 counter=counter+1;
-                l=l+window_size;
+              
             end
-            k=k+window_size;
         end
         temp=inv(transpose(A)*A)*transpose(A)*b;
         u(i,j)=temp(1,1);
         v(i,j)=temp(2,1);
-        end
-        
+        count=count+1; 
+    
     end
+  
 end
+u(isnan(u))=0;
+v(isnan(v))=0;
+count
 end
 
 
