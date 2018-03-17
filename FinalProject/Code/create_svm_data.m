46ddf8e2a62e0dd73ecefd3f02d0386c2fdbd25f
vikrant4k <vikrant4.k@gmail.com>
2018-03-17 23:06:48 +0100

    First SVMCompletion

--- FinalProject/Code/create_svm_data.m
+++ FinalProject/Code/create_svm_data.m
@@ -1,10 +1,18 @@
-function[file_name]=create_svm_data(image_hist_data,num_files,class_index,file_name)
+function[file_name]=create_svm_data(image_hist_data,file_name)
 tot_images=size(image_hist_data,1);
 fileID = fopen(file_name,'w');
-% for i=1:tot_images
-%     str="";
-%     if(i<=50)
-%         str=strcat(str,"+",""
-%     for j=1:size(image_hist_data,2)
-% end 
+for i=1:tot_images
+    str="";
+    if(i<=50)
+        str=strcat(str,"+","1"," ");
+    else
+        str=strcat(str,"-","1"," ");
+    end
+    for j=1:size(image_hist_data,2)
+        str=strcat(str,num2str(j),":",num2str(image_hist_data(i,j))," ");
+    end
+    str=strcat(str,"\n");
+    fprintf(fileID,str);
 end
+fclose(fileID);
+end 
--- FinalProject/Code/main.m
+++ FinalProject/Code/main.m
@@ -26,7 +26,7 @@ for i=1:img_count-1
     assig_count=assig_count+desc_count;
 end
 %% 
-%img_hist_data=gather(img_hist_data);
+img_hist_data=gather(img_hist_data);
 x_axis=zeros(numClusters,1);
 y_axis=zeros(numClusters,1);
 for i=1:numClusters
@@ -37,4 +37,14 @@ end
 %histogram(y_axis,1200)
 %% 
 image_hist_train_data=create_train_data(101,numClusters,centers);
-
+image_hist_test_data=create_train_data(151,numClusters,centers);
+%% 
+file_train_name=create_svm_data(image_hist_train_data,'train.txt');
+file_test_name=create_svm_data(image_hist_test_data,'test.txt');
+%% 
+dirData = '/home/vik1/Downloads/subj/cv/github/Computer-Vision/FinalProject/Code/'; 
+[image_train_label, image_train_data] = libsvmread(fullfile(dirData,'train.txt'));
+model=train(image_train_label,image_train_data, '-c 1 ');
+%% 
+[image_test_label, image_test_data] = libsvmread(fullfile(dirData,'test.txt'));
+[predictions,accuracy,~]=predict(image_test_label, image_test_data,model);
